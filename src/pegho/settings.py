d8ac06ac825c70b5c5291487dc38116513497a69
Gabriel Mira <gllimamira@gmail.com>
2024-10-25 14:55:30 -0300

    mudando as configurações para achar o layout.html

--- src/pegho/settings.py
+++ src/pegho/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "recruitment.apps.RecruitmentConfig",
+    "django_bootstrap5",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -55,7 +58,7 @@ ROOT_URLCONF = "pegho.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "recruitment", "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
